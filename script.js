fe8ebcc7a6474f738bfe9f5d6367c2479c4b486b
sarahandromeda <SarahAndromeda@protonmail.ch>
2022-02-02 19:04:24 -0700

    Created index page, styling page, and script page. Fading checkbox currently does not work.

---
+++ script.js
@@ -0,0 +1,69 @@
+// List of available colors dark to light
+const COLORS = ["#154360", "#1A5276", "#1F618D", "#2471A3", "#2980B9",
+        "#5499C7", "#7FB3D5", "#A9CCE3", "#D4E6F1", "#EAF2F8", "#FFFFFF"];
+const gridContainer = document.querySelector(".grid-container");
+
+const drawGrid = function (size) {
+    for (let i=0; i < (size**2); i++) {
+        const newTile = document.createElement("div");
+        newTile.id = "tile";
+        gridContainer.appendChild(newTile);
+    }
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
+
+const resetGrid = function () {
+    while (gridContainer.lastElementChild) {
+        gridContainer.removeChild(gridContainer.lastElementChild)
+    }
+}
+
+const fadeColor = function (target) {
+    let i = 0;
+    let interval = setInterval(paint, 5000);
+    function paint() {
+        if (i == COLORS.length) {
+            clearInterval(interval);
+        } else {
+            target.style.backgroundColor = COLORS[i];
+            i++;
+        }
+    }
+};
+
+const setFade = function () {
+    const fadeTiles = document.querySelectorAll("#tile");
+    const fadeToggle = document.getElementById("noFade");
+    if (fadeToggle.checked == false) {
+        fadeTiles.forEach( (fadeTile) => {
+            fadeTile.addEventListener("mouseleave", () => {
+                fadeColor(fadeTile)
+            });
+        })
+    };
+}
+
+const setColor = function () {
+    const tiles = document.querySelectorAll("#tile");
+    tiles.forEach( (tile) => {
+        tile.addEventListener("mouseenter", () => {
+            tile.style.backgroundColor = COLORS[0];
+        });
+    })
+};
+
+drawGrid(16);
+setColor();
+setFade();
+
+const changeGrid = document.getElementById("gridSize");
+changeGrid.addEventListener("change", (e) => {
+    if (e.target.value >= 2 && e.target.value <= 100) {
+        resetGrid();
+        drawGrid(e.target.value);
+        setColor();
+        setFade();
+    }
+})
+
+
