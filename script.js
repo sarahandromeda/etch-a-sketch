39d1ec76b4e2f527214b05ee5de03c762e4178a9
sarahandromeda <SarahAndromeda@protonmail.ch>
2022-02-03 19:02:05 -0700

    Add color option buttons. Fixed fade option to enable for more straight forwardness. Need to add listeners to greyscale and rainbow buttons. Other options work for now.

--- script.js
+++ script.js
@@ -19,41 +19,58 @@ const resetGrid = function () {
 }
 
 const fadeColor = function (e) {
-    let i = 0;
+    let i = 1;
     const targetElem = e.target;
-    let interval = setInterval(paint, 1000);
+    let interval = setInterval(paint, 500);
     function paint() {
-        if (i == COLORS.length) {
+        if (i < 0) {
+            targetElem.style.backgroundColor = "#FFF";
+            targetElem.style.opacity = 1;
             clearInterval(interval);
         } else {
-            targetElem.style.backgroundColor = COLORS[i];
-            i++;
+            targetElem.style.opacity = i;
+            i = (i * 10 - 0.2 * 10)/ 10;
         }
     }
 };
 
 const setFade = function () {
     const fadeTiles = document.querySelectorAll("#tile");
-    const fadeToggle = document.getElementById("noFade");
-    if (fadeToggle.checked == false) {
+    const fadeToggle = document.getElementById("enableFade");
+    if (fadeToggle.checked == true) {
         fadeTiles.forEach( (fadeTile) => {
             fadeTile.addEventListener("mouseleave", fadeColor);
         })
-    };
+    } else {
+        fadeTiles.forEach( (fadeTile) => {
+            fadeTile.removeEventListener("mouseleave", fadeColor);
+        })
+    }
+};
+
+const getColor = function () {
+    const colorPicker = document.getElementById("customColor");
+    return colorPicker.value;
 }
 
 const setColor = function () {
+    const pickedColor = getColor();
+    console.log(pickedColor);
     const tiles = document.querySelectorAll("#tile");
     tiles.forEach( (tile) => {
         tile.addEventListener("mouseenter", () => {
-            tile.style.backgroundColor = COLORS[0];
+            tile.style.backgroundColor = pickedColor;
         });
     })
 };
 
 drawGrid(16);
 setColor();
-setFade();
+
+// Listeners and helper fucntions for color options
+
+const customColor = document.getElementById("customColor");
+customColor.addEventListener("change", setColor);
 
 const changeGrid = document.getElementById("gridSize");
 changeGrid.addEventListener("change", (e) => {
@@ -61,23 +78,13 @@ changeGrid.addEventListener("change", (e) => {
         resetGrid();
         drawGrid(e.target.value);
         setColor();
-        setFade();
+        if (enableFade.checked == true) {
+            setFade();
+        }
     }
 })
 
-const disableFade = document.getElementById("noFade");
-disableFade.addEventListener("change", (e) => {
-    if (disableFade.checked == true) {
-        const gridTiles = document.querySelectorAll("#tile");
-        gridTiles.forEach( (elem) => {
-            elem.removeEventListener("mouseleave", fadeColor)
-        })
-    } else {
-        const gridTiles = document.querySelectorAll("#tile");
-        gridTiles.forEach( (elem) => {
-            elem.addEventListener("mouseleave", fadeColor)
-        })
-    }
-})
+const enableFade = document.getElementById("enableFade");
+enableFade.addEventListener("change", setFade);
 
 
