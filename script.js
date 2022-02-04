d9387469425ece49bbcad5a23430ca88917d5d29
sarahandromeda <SarahAndromeda@protonmail.ch>
2022-02-04 16:45:37 -0700

    Added styling to make options look like cards with selected styles. Fixed bug that didn't set clor option when changing grid size.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // List of available colors dark to light
-const COLORS = ["#154360", "#1A5276", "#1F618D", "#2471A3", "#2980B9",
-        "#5499C7", "#7FB3D5", "#A9CCE3", "#D4E6F1", "#EAF2F8", "#FFFFFF"];
+const BRIGHT = ["#E43333", "#E48C33", "#E4E433", "#8CE433", "#33E433",
+        "#33E48C", "#33E4E4", "#338CE4", "#3333E4", "#8C33E4", "#E433E4"];
 const gridContainer = document.querySelector(".grid-container");
 
 const drawGrid = function (size) {
@@ -48,21 +48,58 @@ const setFade = function () {
     }
 };
 
-const getColor = function () {
+const getCustomColor = function () {
     const colorPicker = document.getElementById("customColor");
     return colorPicker.value;
 }
 
 const setColor = function () {
-    const pickedColor = getColor();
-    console.log(pickedColor);
-    const tiles = document.querySelectorAll("#tile");
-    tiles.forEach( (tile) => {
-        tile.addEventListener("mouseenter", () => {
-            tile.style.backgroundColor = pickedColor;
-        });
-    })
+    const currentSetting = document.querySelector(".selected");
+    if (currentSetting.firstElementChild.id == "customColor") {
+        const pickedColor = getCustomColor();
+        console.log(pickedColor);
+        const tiles = document.querySelectorAll("#tile");
+        tiles.forEach( (tile) => {
+            tile.addEventListener("mouseenter", () => {
+                tile.style.backgroundColor = pickedColor;
+            });
+        })
+    } else if (currentSetting.firstElementChild.id == "greyscale") {
+        //
+    } else if (currentSetting.firstElementChild.id == "rainbow") {
+        setRainbow();
+    }
 };
+
+const getRainbowColor = function () {
+    let listLength = BRIGHT.length;
+    let randomChoice = Math.floor(Math.random() * listLength);
+    return BRIGHT[randomChoice];
+};
+
+
+const setRainbow = function () {
+    const rainbowTiles = document.querySelectorAll("#tile");
+    rainbowTiles.forEach( (rainbowTile) => {
+        rainbowTile.addEventListener("mouseenter", () => {
+            rainbowTile.style.backgroundColor = getRainbowColor();
+        })
+    })
+}
+
+const selectedOn = function (e) {
+    selectedOff();
+    const div = e.target.parentElement;
+    console.log(div);
+    div.classList.add("selected");
+}
+
+const selectedOff = function () {
+    const element = document.querySelector(".selected");
+    if (element) {
+        element.classList.remove("selected");
+    }
+}
 
 drawGrid(16);
 setColor();
@@ -71,6 +108,11 @@ setColor();
 
 const customColor = document.getElementById("customColor");
 customColor.addEventListener("change", setColor);
+customColor.addEventListener("click", selectedOn);
+
+const rainbowColor = document.getElementById("rainbow");
+rainbowColor.addEventListener("click", setRainbow);
+rainbowColor.addEventListener("click", selectedOn);
 
 const changeGrid = document.getElementById("gridSize");
 changeGrid.addEventListener("change", (e) => {
