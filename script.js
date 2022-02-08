f997b0209380cf845430a5abb688bf10dd50159e
sarahandromeda <SarahAndromeda@protonmail.ch>
2022-02-08 14:38:56 -0700

    Finished greyscale option and fixed fading bugs.

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const BRIGHT = ["#E43333", "#E48C33", "#E4E433", "#8CE433", "#33E433",
         "#33E48C", "#33E4E4", "#338CE4", "#3333E4", "#8C33E4", "#E433E4"];
 const gridContainer = document.querySelector(".grid-container");
 
+// Boolean of options to toggle for event listener removal
+let customSet = true;
+let rainbowSet = false;
+let greyscaleSet = false;
+let fadeSet = false;
+
 const drawGrid = function (size) {
     for (let i=0; i < (size**2); i++) {
         const newTile = document.createElement("div");
@@ -15,6 +21,78 @@ const drawGrid = function (size) {
 const resetGrid = function () {
     while (gridContainer.lastElementChild) {
         gridContainer.removeChild(gridContainer.lastElementChild)
+    }
+}
+
+const setColor = function () {
+    const currentSetting = document.querySelector(".selected");
+    if (currentSetting.firstElementChild.id == "customColor") {
+        goCustomColor();
+    } else if (currentSetting.firstElementChild.id == "greyscale") {
+        goGreyscale();
+    } else if (currentSetting.firstElementChild.id == "rainbow") {
+        goRainbow();
+    }
+};
+
+const setCustomColor = function (e) {
+    let pickedColor = document.getElementById("customColor");
+    pickedColor = pickedColor.value;
+    e.target.style.backgroundColor = pickedColor;
+}
+
+const goCustomColor = function () {
+    const tiles = document.querySelectorAll("#tile");
+    removeListeners(tiles);
+    customSet = true;
+    tiles.forEach( (tile) => {
+        // tile.style.backgroundColor = "#FFFFFF";
+        tile.addEventListener("mouseenter", setCustomColor);
+    })
+}
+
+const setGreyscale = function (e) {
+    e.target.style.backgroundColor = "#000000"
+    let opacity = e.target.style.opacity;
+    if (opacity < 1) {
+        e.target.style.opacity = ((opacity * 10) + (0.1 * 10))/ 10;
+    }
+}
+
+const goGreyscale = function () {
+    const greyTiles = document.querySelectorAll("#tile");
+    removeListeners(greyTiles);
+    greyscaleSet = true;
+    greyTiles.forEach( (greyTile) => {
+        greyTile.style.opacity = 0;
+        greyTile.addEventListener("mouseenter", setGreyscale);
+    })
+};
+
+const setRainbowColor = function (e) {
+    let listLength = BRIGHT.length;
+    let randomChoice = Math.floor(Math.random() * listLength);
+    const randomColor = BRIGHT[randomChoice];
+    e.target.style.backgroundColor = randomColor;
+};
+
+const goRainbow = function () {
+    const rainbowTiles = document.querySelectorAll("#tile");
+    removeListeners(rainbowTiles);
+    rainbowSet = true;
+    rainbowTiles.forEach( (rainbowTile) => {
+        rainbowTile.addEventListener("mouseenter", setRainbowColor);
+    })
+}
+
+const goGridSize = function (e) {
+    if (e.target.value >= 2 && e.target.value <= 100) {
+        resetGrid();
+        drawGrid(e.target.value);
+        setColor();
+        if (enableFade.checked == true) {
+            setFade();
+        }
     }
 }
 
@@ -35,57 +113,23 @@ const fadeColor = function (e) {
 };
 
 const setFade = function () {
+    resetGrid();
+    drawGrid(changeGrid.value);
+    setColor();
     const fadeTiles = document.querySelectorAll("#tile");
     const fadeToggle = document.getElementById("enableFade");
-    if (fadeToggle.checked == true) {
+    if (fadeToggle.checked == true && fadeToggle.disabled == false) {
         fadeTiles.forEach( (fadeTile) => {
+            fadeSet = true;
             fadeTile.addEventListener("mouseleave", fadeColor);
         })
     } else {
         fadeTiles.forEach( (fadeTile) => {
+            fadeSet = false;
             fadeTile.removeEventListener("mouseleave", fadeColor);
         })
     }
 };
-
-const getCustomColor = function () {
-    const colorPicker = document.getElementById("customColor");
-    return colorPicker.value;
-}
-
-const setColor = function () {
-    const currentSetting = document.querySelector(".selected");
-    if (currentSetting.firstElementChild.id == "customColor") {
-        const pickedColor = getCustomColor();
-        console.log(pickedColor);
-        const tiles = document.querySelectorAll("#tile");
-        tiles.forEach( (tile) => {
-            tile.addEventListener("mouseenter", () => {
-                tile.style.backgroundColor = pickedColor;
-            });
-        })
-    } else if (currentSetting.firstElementChild.id == "greyscale") {
-        //
-    } else if (currentSetting.firstElementChild.id == "rainbow") {
-        setRainbow();
-    }
-};
-
-const getRainbowColor = function () {
-    let listLength = BRIGHT.length;
-    let randomChoice = Math.floor(Math.random() * listLength);
-    return BRIGHT[randomChoice];
-};
-
-
-const setRainbow = function () {
-    const rainbowTiles = document.querySelectorAll("#tile");
-    rainbowTiles.forEach( (rainbowTile) => {
-        rainbowTile.addEventListener("mouseenter", () => {
-            rainbowTile.style.backgroundColor = getRainbowColor();
-        })
-    })
-}
 
 const selectedOn = function (e) {
     selectedOff();
@@ -101,32 +145,68 @@ const selectedOff = function () {
     }
 }
 
+// Helper function to remove any previous option listeners
+const removeListeners = function (targetTiles) {
+    if (customSet == true) {
+        customSet = false;
+        targetTiles.forEach ( (targetTile) => {
+            targetTile.removeEventListener("mouseenter", setCustomColor);
+        })
+    } else if (greyscaleSet == true) {
+        greyscaleSet = false;
+        targetTiles.forEach( (targetTile) => {
+            targetTile.style.backgroundColor = "#FFFFFF";
+            targetTile.style.opacity = 1;
+            targetTile.removeEventListener("mouseenter", setGreyscale);
+        })
+    } else if (rainbowSet == true) {
+        rainbowSet = false;
+        targetTiles.forEach( (targetTile) => {
+            targetTile.removeEventListener("mouseenter", setRainbowColor);
+        })
+    } else if (fadeSet == true && enableFade.checked == false) {
+        fadeSet = false;
+        targetTiles.forEach( (targetTile) => {
+            targetTile.removeEventListener("mouseleave", fadeColor);
+        })
+    }
+}
+
 drawGrid(16);
 setColor();
 
 // Listeners and helper fucntions for color options
 
-const customColor = document.getElementById("customColor");
-customColor.addEventListener("change", setColor);
-customColor.addEventListener("click", selectedOn);
-
-const rainbowColor = document.getElementById("rainbow");
-rainbowColor.addEventListener("click", setRainbow);
-rainbowColor.addEventListener("click", selectedOn);
-
-const changeGrid = document.getElementById("gridSize");
-changeGrid.addEventListener("change", (e) => {
-    if (e.target.value >= 2 && e.target.value <= 100) {
-        resetGrid();
-        drawGrid(e.target.value);
-        setColor();
-        if (enableFade.checked == true) {
-            setFade();
-        }
-    }
-})
-
 const enableFade = document.getElementById("enableFade");
 enableFade.addEventListener("change", setFade);
+
+const customColor = document.querySelector(".custom");
+customColor.addEventListener("click", goCustomColor);
+customColor.addEventListener("click", selectedOn);
+customColor.addEventListener("click", () => {
+    enableFade.disabled = false;
+})
+
+const greyscaleColor = document.querySelector(".grey");
+greyscaleColor.addEventListener("click", goGreyscale);
+greyscaleColor.addEventListener("click", selectedOn);
+greyscaleColor.addEventListener("click", () => {
+    enableFade.checked = false;
+    enableFade.disabled = true;
+    setFade();
+})
+
+const rainbowColor = document.querySelector(".rainbow");
+rainbowColor.addEventListener("click", goRainbow);
+rainbowColor.addEventListener("click", selectedOn);
+rainbowColor.addEventListener("click", () => {
+    enableFade.disabled = false;
+})
+
+const changeGrid = document.getElementById("gridSize");
+changeGrid.addEventListener("change", goGridSize);
+changeGrid.addEventListener("click", () => {
+    enableFade.disabled = false;
+})
 
 
